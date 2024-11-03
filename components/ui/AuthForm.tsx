67b744431a47fcfc7ebf11555fafd563312b2bde
ilyas <ilyasouerdi2@gmail.com>
2024-11-03 15:24:55 +0100

    appwrite

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -21,6 +21,8 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
+
 
 
 
@@ -43,14 +45,14 @@ const AuthForm = ({ type }: { type: string }) => {
         setisLoading(true);
         try{
             if(type === 'sign-up'){
-                //const newUser = await SignUp(data);
-                //setUser(newUser);
+                const newUser = await signUp(data);
+                setUser(newUser);
             }
             if(type === 'sign-in'){
-                //const response = await SignIn({email: data.email,
-                  //  password: data.password,}
-                //);
-                //if(response) router.push('/');
+                const response = await signIn({email: data.email,
+                    password: data.password,}
+                );
+                if(response) router.push('/');
             }
         }catch(error){
             console.log(error);
